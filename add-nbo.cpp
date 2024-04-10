6cf99076eeb031e917f7561972021ae79add360a
keryzy <ycseung00@gmail.com>
2024-04-10 00:27:49 -0400

    Update add-nbo.cpp (Error handling)

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -2,6 +2,14 @@
 #include <stdint.h>
 #include <netinet/in.h> //for htonl
 
+long getFileSize(FILE* file){
+	long currentPosition = ftell(file);
+	fseek(file, 0, SEEK_END);
+    	long fileSize = ftell(file);
+    	fseek(file, currentPosition, SEEK_SET);
+    	return fileSize;
+}
+
 int main(int argc, char* argv[]) {
 	FILE *five_hundred;
 	FILE *thousand;
@@ -9,6 +17,19 @@ int main(int argc, char* argv[]) {
 
 	thousand = fopen(argv[1],"r");
 	five_hundred = fopen(argv[2], "r");
+
+	if(thousand == NULL | five_hundred == NULL){
+		printf("File open error");
+		return 1;
+	}
+
+	long thousand_size = getFileSize(thousand);
+	long five_hundred_size = getFileSize(five_hundred);
+	
+	if(thousand_size < 4 | five_hundred_size < 4){
+		printf("File size error");
+		return 1;
+	}
 
 	//Notion
 	fread(&num1, sizeof(uint32_t), 1, thousand);
