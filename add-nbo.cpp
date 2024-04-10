8cad950af78ab74dc1acbc2a26bb2adf4526a79e
keryzy <ycseung00@gmail.com>
2024-04-09 23:56:04 -0400

    Add add-nbo project

---
+++ add-nbo.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <stdint.h>
+#include <netinet/in.h> //for htonl
+
+int main(int argc, char* argv[]) {
+	FILE *five_hundred;
+	FILE *thousand;
+	uint32_t num1, num2, result;
+
+	thousand = fopen(argv[1],"r");
+	five_hundred = fopen(argv[2], "r");
+
+	//Notion
+	fread(&num1, sizeof(uint32_t), 1, thousand);
+	fread(&num2, sizeof(uint32_t), 1, five_hundred);
+
+	// little-endian -> big-endian 
+	// num2 0xe803 -> 0x03e8
+	// num1 0xf401 -> 0x01f4
+	num1 = htonl(num1);
+	num2 = htonl(num2);
+
+	result = num1 + num2;
+
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", num1, num1, num2, num2, result, result);
+
+	fclose(five_hundred);
+	fclose(thousand);
+}
